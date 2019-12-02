c771ebf260f818c445c8737a6a2ed2439bfe6e93
syamgith <syamgithksudheesh@gmail.com>
2019-12-02 13:01:51 +0530

    Declared and initialised buttons and numbers

--- app/src/main/java/com/example/calculatorapp/MainActivity.java
+++ app/src/main/java/com/example/calculatorapp/MainActivity.java
@@ -3,12 +3,28 @@ package com.example.calculatorapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btnadd, btnsub, btnmult, btndiv ;
+    EditText num1,num2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnadd = findViewById(R.id.btnadd);
+        btnsub = findViewById(R.id.btnsub);
+        btnmult = findViewById(R.id.btnmult);
+        btndiv = findViewById(R.id.btndiv);
+
+        num1 = findViewById(R.id.num1);
+        num2 = findViewById(R.id.num2);
+
+
+
     }
 }
